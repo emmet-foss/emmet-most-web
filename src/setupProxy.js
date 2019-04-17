e690df2631b7d8071015d26b642368b4f9c0888f
East <eastmaels@gmail.com>
2019-04-18 01:05:51 +0900

    remove dotenv from setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,5 @@
-require('dotenv').config()
 const proxy = require('http-proxy-middleware');
-const API_HOST = process.env.API_HOST
+
 module.exports = function(app) {
-  app.use(proxy('/api', { target: API_HOST }));
+  app.use(proxy('/api', { target: 'https://emmet-mos-api.herokuapp.com/' }));
 };
