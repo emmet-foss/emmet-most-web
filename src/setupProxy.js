a6fd13e0b524b4185b59e6c470f32b3804fb1e59
East <eastmaels@gmail.com>
2019-04-18 00:57:34 +0900

    get proxy host from .env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const proxy = require('http-proxy-middleware');
-
+const API_HOST = process.env.API_HOST
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'https://emmet-mos-api.herokuapp.com/' }));
+  app.use(proxy('/api', { target: API_HOST }));
 };
