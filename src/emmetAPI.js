f1c168d9b41e3f7752eb2a25280159a7e543f05a
East <eastmaels@gmail.com>
2019-04-18 23:36:17 +0900

    pre-fix env variables with REACT_APP_

--- src/emmetAPI.js
+++ src/emmetAPI.js
@@ -1,9 +1,7 @@
 import Client from './helpers/client'
 require('dotenv').config()
 
-const emmetUrl = process.env.EMMET_API || 'http://localhost:3001';
-console.log(emmetUrl)
-
+const emmetUrl = process.env.REACT_APP_EMMET_API || 'http://localhost:3001';
 const api = new Client(emmetUrl);
 
 export default api;
