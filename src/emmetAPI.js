07c0be2e65275ba1cffc3101a24e1880727693de
East <eastmaels@gmail.com>
2019-04-18 22:47:43 +0900

    use dotenv

--- src/emmetAPI.js
+++ src/emmetAPI.js
@@ -1,5 +1,8 @@
 import Client from './helpers/client'
+require('dotenv').config()
+
 const emmetUrl = process.env.EMMET_API || 'http://localhost:3001';
+console.log(emmetUrl)
 
 const api = new Client(emmetUrl);
 
