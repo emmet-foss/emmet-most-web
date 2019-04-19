9861cd8546de96668a39b140a19b394e89c628d8
East <eastmaels@gmail.com>
2019-04-19 22:00:28 +0900

    add antd; apply antd layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { DatePicker, Layout, Menu, Breadcrumb, Icon } from 'antd';
 import emmetAPI from './emmetAPI';
+import 'antd/dist/antd.css';
 import './App.css';
+
+const {
+  Header, Content, Footer, Sider,
+} = Layout;
 
 class App extends Component {
   state = {
     response: '',
     name: '',
     location: '',
-    responseToPost: ''
+    responseToPost: '',
+    collapsed: false,
   };
+
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  }
 
   componentDidMount() {
     this.callApi()
@@ -19,6 +31,7 @@ class App extends Component {
   callApi = async () => {
     const response = await emmetAPI.getUrl('/api/v1/stores');
     const body = await response.json();
+    console.log('body', body)
     if (response.status !== 200) throw Error(body.message);
     return body;
   };
@@ -40,32 +53,73 @@ class App extends Component {
   };
 
   render() {
+
     return (
-      <div className="App">
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Store Name:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={e => this.setState({ name: e.target.value })}
-          />
-          <p>
-            <strong>Store Location:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.location}
-            onChange={e => this.setState({ location: e.target.value })}
-          />
-          <p>
-            <button type="submit">Submit</button>
-          </p>
-        </form>
-        <p>{this.state.responseToPost}</p>
-      </div>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Stores</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Menus</span>
+            </Menu.Item>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>Orders</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+
+            < div className="App">
+                <p>{this.state.response}</p>
+                <form onSubmit={this.handleSubmit}>
+                  <p>
+                    <strong>Store Name:</strong>
+                  </p>
+                  <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={e => this.setState({ name: e.target.value })}
+                  />
+                  <p>
+                    <strong>Store Location:</strong>
+                  </p>
+                  <input
+                    type="text"
+                    value={this.state.location}
+                    onChange={e => this.setState({ location: e.target.value })}
+                  />
+                  <p>
+                    <button type="submit">Submit</button>
+                  </p>
+                </form>
+                <p>{this.state.responseToPost}</p>
+                <DatePicker />
+              </div>
+
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
